f725eebb53a6b7a94ad42ae98b00534d39db2204
wdjpng <muenzel.lukas@gmail.com>
2019-07-22 18:02:46 +0200

    Added google sign in

--- lib/settingsPage.dart
+++ lib/settingsPage.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
-import 'selectorPage.dart'
-;
+import 'selectorPage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 class SettingsPage extends StatefulWidget {
   @override
   _SettingsPageState createState() => new _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
 
   @override
   void initState() {
     super.initState();
   }
 
-  void _openSelector(BuildContext){
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => SelectorPage()));
+  void _openSelector(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => SelectorPage()));
+  }
+
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
   }
 
   @override
@@ -26,9 +41,20 @@ class _SettingsPageState extends State<SettingsPage> {
         elevation: 0.0,
       ),
       body: new ListView(
-        children: <Widget>[new RaisedButton(onPressed: () => _openSelector(context), child: const Text(
-            'Fàcher auswählen',
-        ),)],
+        children: <Widget>[
+          new RaisedButton(
+            onPressed: () => _openSelector(context),
+            child: const Text(
+              'Fàcher auswählen',
+            ),
+          ),
+          new RaisedButton(
+            onPressed: () => _handleSignIn(),
+            child: const Text(
+              'Als Administrator anmelden',
+            ),
+          )
+        ],
       ),
     );
   }
