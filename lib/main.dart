629f40106123a1232e96fe9c1c834abf80a3afe0
wdjpng <muenzel.lukas@gmail.com>
2019-07-31 10:17:42 +0200

    Forced the portrait orientation for the device

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:calendar1/pages/eventsPage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   initializeDateFormatting().then((_) => runApp(AgendaApp()));
@@ -9,6 +10,10 @@ void main() {
 class AgendaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'AgendaApp',
       theme: ThemeData(
--- lib/pages/eventsPage.dart
+++ lib/pages/eventsPage.dart
@@ -76,8 +76,8 @@ class EventsPageState extends State<EventsPage> with TickerProviderStateMixin {
     setState(() {
       visibleEvents = Map.fromEntries(
         eventsAsStrings.entries.where(
-          (entry) =>
-              entry.key.isAfter(first.subtract(const Duration(days: 1))) &&
+              (entry) =>
+          entry.key.isAfter(first.subtract(const Duration(days: 1))) &&
               entry.key.isBefore(last.add(const Duration(days: 1))),
         ),
       );
@@ -100,8 +100,8 @@ class EventsPageState extends State<EventsPage> with TickerProviderStateMixin {
       context,
       MaterialPageRoute(
           builder: (context) => EventViewerPage(
-                data: data,
-              )),
+            data: data,
+          )),
     );
   }
 
@@ -134,8 +134,8 @@ class EventsPageState extends State<EventsPage> with TickerProviderStateMixin {
         context,
         MaterialPageRoute(
             builder: (context) => EventViewerPage(
-                  data: data,
-                )),
+              data: data,
+            )),
       );
     } else {
       key.currentState.showSnackBar(new SnackBar(
@@ -229,7 +229,7 @@ class EventsPageState extends State<EventsPage> with TickerProviderStateMixin {
       ),
       headerStyle: HeaderStyle(
         formatButtonTextStyle:
-            TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
         formatButtonDecoration: BoxDecoration(
           color: Colors.deepOrange[400],
           borderRadius: BorderRadius.circular(16.0),
@@ -244,17 +244,17 @@ class EventsPageState extends State<EventsPage> with TickerProviderStateMixin {
     return ListView(
       children: _selectedEvents
           .map((event) => Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0.8),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                child: ListTile(
-                  title: Text(event.toString()),
-                  onTap: () => onEventPressed(context, event),
-                ),
-              ))
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.8),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        margin:
+        const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: ListTile(
+          title: Text(event.toString()),
+          onTap: () => onEventPressed(context, event),
+        ),
+      ))
           .toList(),
     );
   }
